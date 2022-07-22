f30c7b93f485201f5c3e34c6c21f32b25370dc09
G-who0212 <gwho990212@gmail.com>
2022-07-22 13:39:58 +0900

    modUserInfo clear

--- flowerProject/api/urls.py
+++ flowerProject/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import LoginView, RegisterView
+from .views import LoginView, MyPageAPIView, RegisterView, UserAddressAPIView, UserDeleteAPIView
 
 urlpatterns = [
     path('userinfo/signup/', RegisterView.as_view()),
-    path('userinfo/', LoginView.as_view()),
+    path('userinfo/login/', LoginView.as_view()),
+    path('userinfo/', MyPageAPIView.as_view()),
+    path('userifo/delete/', UserDeleteAPIView.as_view()),
+    path('userinfo/address/', UserAddressAPIView.as_view())
 ]
--- flowerProject/api/views.py
+++ flowerProject/api/views.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import AbstractUser
 from customer.models import UserInfo
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer, MyPageSerializer, MyAddressSerializer
 
 class RegisterView(generics.CreateAPIView): # 회원가입
     queryset = UserInfo.objects.all()
@@ -20,4 +21,43 @@ class LoginView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         token = serializer.validated_data
         return Response({"token": token.key}, status=status.HTTP_200_OK)
+    
+    
+class MyPageAPIView(APIView):
+    def get(self, request):
+        if request.user:
+            myinfo = UserInfo.objects.filter(username = request.user.username)
+            serializer = MyPageSerializer(myinfo, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    
+    def put(self, request):
+        if request.user:
+            myinfo = UserInfo.objects.filter(username = request.user.username)
+            serializer = MyPageSerializer(myinfo, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+class UserDeleteAPIView(APIView):
+    def delete(self, request):
+        if request.user:
+            myinfo = UserInfo.objects.filter(username = request.user.username)
+            myinfo.delete()
+            return Response("UserInfo successfully deleted!")
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+class UserAddressAPIView(APIView):
+    def get(self, request):
+        if request.user:
+            myinfo = UserInfo.objects.filter(username = request.user.username)
+            serializer = MyAddressSerializer(myinfo, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 ##############################################
