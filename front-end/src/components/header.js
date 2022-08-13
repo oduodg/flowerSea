cb5d3a01c3a31ecc17c281a56f1a1a85f4a21972
oduodg <werewildfire@gmail.com>
2022-08-13 16:40:57 +0900

    header 구현

---
+++ front-end/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function header() {
+	return (
+		<>
+		<div className="relative z-10 w-full shadow h-11">
+			<img className="pt-1 ml-10 h-9" alt="signature" src="/images/signature_temporary.png"></img>
+		</div>
+		</>
+	)
+}
