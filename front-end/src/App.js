64902c955982eb5f034d5132c9c295f42d68e777
oduodg <werewildfire@gmail.com>
2022-08-16 19:53:40 +0900

    React Router - Main, Login, Register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Main, Login, Register } from './pages';
+import Header from './components/header';
 
 export default function App() {
   return (
-    <>
-      <h1>Hello, flowerSea</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} exact={true} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function header() {
 	return (
 		<>
-		<div className="relative z-10 w-full shadow h-11">
-			<img className="pt-1 ml-10 h-9" alt="signature" src="/images/signature_temporary.png"></img>
-		</div>
+			<div className="relative z-10 w-full shadow h-11">
+				<Link to="/">
+				<img className="inline pt-1 ml-10 h-9" alt="signature" src="/images/signature_temporary.png"></img>
+				</Link>
+				<Link to="/login">
+				<button className="float-right p-1 mt-1 mr-10 border-2 rounded-lg">로그인</button>
+				</Link>
+			</div>
 		</>
 	)
-}
+};
