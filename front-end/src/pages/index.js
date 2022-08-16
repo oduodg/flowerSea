c47431556e4fd0bfe464baa422e180b843ee453d
oduodg <werewildfire@gmail.com>
2022-08-17 02:22:17 +0900

    page export

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,3 +1,6 @@
 export { default as Main } from './Main';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
+export { default as Logout } from './Logout';
+export { default as Cart } from './Cart';
+export { default as MyPage } from './MyPage';
