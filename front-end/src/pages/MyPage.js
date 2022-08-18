879b4d087a1aa1ed8411395bdd926152cb54e988
Jaewoo <sjwlion@naver.com>
2022-08-18 17:59:55 +0900

    MyPage

--- front-end/src/pages/MyPage.js
+++ front-end/src/pages/MyPage.js
@@ -1,145 +1,151 @@
-import React from 'react'
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
-import { XIcon } from '@heroicons/react/outline'
-import { ChevronDownIcon, FilterIcon, MinusSmIcon, PlusSmIcon, ViewGridIcon } from '@heroicons/react/solid'
+import React, {useState} from 'react'
+import './MyPage.css';
+import { Link } from 'react-router-dom';
 
 
-const subCategories = [
-  { name: '내 정보', href: '#' },
-  { name: '주문 내역', href: '#' },
+export default function MyPage() {
 
-]
+  const [Name, setName] = useState("")
+  const [Id, setId] = useState("")
+  const [Password, setPassword] = useState("")
+  const [PasswordCheck, setPasswordCheck] = useState("")
+  const [Address, setAddress] = useState("")
+  const [PhoneNum, setPhoneNum] = useState("")
+  const [Email, setEmail] = useState("")
+  const [x, setX] = useState([]);
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value)
+  }
 
-export default function Example() {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+  const onIdHandler = (event) => {
+    setId(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const onPasswordCheckHandler = (event) => {
+    setPasswordCheck(event.currentTarget.value)
+  }
+
+  const onAddressHandler = (event) => {
+    setAddress(event.currentTarget.value)
+  }
+
+  const onPhoneNumHandler = (event) => {
+    setPhoneNum(event.currentTarget.value)
+  }
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  }
+
+
+  const HandleClickRadioButton = (e) => {
+    console.log(e.target.value)
+    setX(e.target.value)
+  }
 
   return (
-    <div className="bg-white">
-      <div>
-        {/* Mobile filter dialog */}
-        <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-40 lg:hidden" onClose={setMobileFiltersOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh', overflow:'scroll'
+      }}
+  >
+      <form 
+          class="p-10 bg-white rounded-xl space-y-5" 
+          action=""
+          style={{ display: 'flex', flexDirection: 'column'}}
+          onSubmit={onSubmitHandler}
+      >
+        <h1 class="register">마이페이지 - 내정보
+          <span style={{color:'#f34336', fontSize:10, paddingLeft:10}}> </span>
+        </h1>
+        <div class="registerbd"></div>
+        <h3 class="rgName">이름
+        </h3>
+        <div>
+            <input 
+              class="NameWrite" 
+              type='text' 
+              name='Name' 
+              vlaue={Name} 
+              onChange={onNameHandler} 
+              placeholder = ""
+              />
+        </div>
+        <h3 class="rgID">아이디
+        </h3>
+        <div>
+            <input 
+              class="idwrite" 
+              type='text' 
+              name='Id' 
+              value={Id} 
+              onChange={onIdHandler} 
+              placeholder = ""
+              />
+        </div>
+      
+      
+        <h3 class="rgAddress">주소
+        </h3>
+        <div>
+            <input 
+              class="AddressWrite" 
+              type='text' 
+              name='Address' 
+              value={Address} 
+              onChange={onAddressHandler} 
+              placeholder = ""
+              />
+        </div>
+        <h3 class="rgPhoneNum">전화번호
+        </h3>
+        <div>
+            <input 
+              class="PhoneNumWrite" 
+              type='text' 
+              name='PhoneNum' 
+              value={PhoneNum} 
+              onChange={onPhoneNumHandler} 
+              placeholder = ""
+              />
+        </div>
+        
+        <div class="sidemenu">마이페이지</div>
+        
+        
+        <div class="MyInfo">
+          <Link to="/mypage">
+          <button class="GoMyPage">내 정보</button>
+          </Link>
+        </div>
 
-            <div className="fixed inset-0 flex z-40">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="ml-auto relative max-w-xs w-full h-full bg-white shadow-xl py-4 pb-12 flex flex-col overflow-y-auto">
-                  <div className="px-4 flex items-center justify-between">
-                    <h2 className="text-lg font-medium text-gray-900">Filters</h2>
-                    <button
-                      type="button"
-                      className="-mr-2 w-10 h-10 bg-white p-2 rounded-md flex items-center justify-center text-gray-400"
-                      onClick={() => setMobileFiltersOpen(false)}
-                    >
-                      <span className="sr-only">Close menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-
-                  {/* Filters */}
-                  <form className="mt-4 border-t border-gray-200">
-                    <h3 className="sr-only">Categories</h3>
-                    <ul role="list" className="font-medium text-gray-900 px-2 py-3">
-                      {subCategories.map((category) => (
-                        <li key={category.name}>
-                          <a href={category.href} className="block px-2 py-3">
-                            {category.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </form>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="relative z-10 flex items-baseline justify-between pt-12 pb-6 border-b border-gray-200">
-            <h1 className="text-xl font-bold tracking-tight text-gray-900">마이페이지</h1>
-
-            <div className="flex items-center">
-              <Menu as="div" className="relative inline-block text-left">
+        <div class="OrderTable">
+          <Link to="/ordertable">
+          <button class="GoOrderTable">주문 목록</button>
+          </Link>
+        </div>
 
 
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"> */}
-                  </Menu.Items>
-                </Transition>
-              </Menu>
 
-              <button
-                type="button"
-                className="p-2 -m-2 ml-4 sm:ml-6 text-gray-400 hover:text-gray-500 lg:hidden"
-                onClick={() => setMobileFiltersOpen(true)}
-              >
-                <span className="sr-only">Filters</span>
-                <FilterIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-            </div>
-          </div>
-
-          <section aria-labelledby="products-heading" className="pt-6 pb-24">
-            <h2 id="products-heading" className="sr-only">
-              Products
-            </h2>
-
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-10">
-              {/* Filters */}
-              <form className="hidden lg:block">
-                <h3 className="sr-only">Categories</h3>
-                <ul role="list" className="text-sm font-medium text-gray-900 space-y-4 pb-6 ">
-                  {subCategories.map((category) => (
-                    <li key={category.name}>
-                      <a href={category.href}>{category.name}</a>
-                    </li>
-                  ))}
-                </ul>
-
-              </form>
-
-              {/* Product grid */}
-              <div className="lg:col-span-3">
-                {/* Replace with your content */}
-                {/* /End replace */}
-              </div>
-            </div>
-          </section>
-        </main>
-      </div>
+        
+        <div class="rgbt">
+          <button class="rgbutton">수정하기</button>
+        </div>
+      </form>
     </div>
+
+    
   )
+
 }
+
 
