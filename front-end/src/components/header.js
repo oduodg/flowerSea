9ebe80270e9c60f42b4b1e759f3dada6ffeec884
oduodg <werewildfire@gmail.com>
2022-08-18 13:47:20 +0900

    :art: Add profile component
    헤더 컴포넌트 분기 - 프로필 컴포넌트

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Profile from './profile';
 
-export default function header() {
+export default function Header() {
 	return (
 		<>
-			<div className="relative z-10 w-full shadow h-11">
+			<div className="relative z-20 w-full shadow-md h-11">
 				<Link to="/">
 					<img className="inline ml-10 h-11" alt="signature" src="/images/signature_header.png" />
 				</Link>
-				<div className="flex justify-center float-right">
-					<Link to="/login">
-						<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">로그인을 해주세요.</div>
-					</Link>
-					<Link to="/login">
-						<img className="pt-2 mr-4 h-9" alt="profile" src="/images/icon_profile.png" />
-					</Link>
-					<Link to="/cart">
-					<img className="pt-2 mr-10 h-9" alt="profile" src="/images/icon_cart_color.png" />
-					</Link>
-				</div>
+				<Profile/>
 			</div>
 		</>
 	)
---
+++ front-end/src/components/profile.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { CheckToken } from '../auth/CheckToken';
+
+export default function Profile() {
+	const location = useLocation();
+	const { isAuth } = CheckToken(location.key);
+	
+	if (isAuth === 'Success') {
+
+		return (
+			<div className="flex justify-center float-right">
+				<Link to="/mypage">
+					<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">김사자님</div>
+				</Link>
+				<Link to="/mypage">
+					<img className="pt-2 mr-4 h-9" alt="profile" src="/images/icon_profile.png" />
+				</Link>
+				<Link to="/cart">
+					<img className="pt-2 mr-10 h-9" alt="profile" src="/images/icon_cart_color.png" />
+				</Link>
+			</div>
+		)
+	}
+	return (
+		<div className="flex justify-center float-right">
+			<Link to="/login">
+				<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">로그인을 해주세요.</div>
+			</Link>
+			<Link to="/login">
+				<img className="pt-2 mr-4 h-9" alt="profile" src="/images/icon_profile.png" />
+			</Link>
+			<Link to="/cart">
+				<img className="pt-2 mr-10 h-9" alt="profile" src="/images/icon_cart_color.png" />
+			</Link>
+		</div>
+	)
+
+}
