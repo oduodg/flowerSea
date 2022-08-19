d0524451af0216bd1eba2902a2ef476855ff1de2
oduodg <werewildfire@gmail.com>
2022-08-19 22:20:42 +0900

    :art: Update MyPage and OrderTable
    필요없는 코드 대폭 삭제

--- front-end/src/pages/MyPage.js
+++ front-end/src/pages/MyPage.js
@@ -1,143 +1,47 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './MyPage.css';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 export default function MyPage() {
-
-  const [Name, setName] = useState("")
-  const [Id, setId] = useState("")
-  const [Password, setPassword] = useState("")
-  const [PasswordCheck, setPasswordCheck] = useState("")
-  const [Address, setAddress] = useState("")
-  const [PhoneNum, setPhoneNum] = useState("")
-  const [Email, setEmail] = useState("")
-  const [x, setX] = useState([]);
-
-  const onNameHandler = (event) => {
-    setName(event.currentTarget.value)
-  }
-
-  const onIdHandler = (event) => {
-    setId(event.currentTarget.value)
-  }
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value)
-  }
-
-  const onPasswordCheckHandler = (event) => {
-    setPasswordCheck(event.currentTarget.value)
-  }
-
-  const onAddressHandler = (event) => {
-    setAddress(event.currentTarget.value)
-  }
-
-  const onPhoneNumHandler = (event) => {
-    setPhoneNum(event.currentTarget.value)
-  }
-
-  const onEmailHandler = (event) => {
-    setEmail(event.currentTarget.value)
-  }
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-  }
-
-
-  const HandleClickRadioButton = (e) => {
-    console.log(e.target.value)
-    setX(e.target.value)
+  const domain = "http://192.168.35.160:8080/";
+  const userInfo = async () => {
+    try {
+      const res = await axios.get(domain + "api/userinfo/", {
+        headers: `Token ${localStorage.getItem("userToken")}`
+      });
+      localStorage.setItem("userName", res.data.name);
+      localStorage.setItem("PhoneNum", res.data.phoneNum);
+      localStorage.setItem("address", res.data.address);
+    } catch (err) {
+      console.log("Server Error");
+    }
   }
 
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh', overflow: 'scroll'
-    }}
-    >
-      <form
-        className="p-10 space-y-5 bg-white rounded-xl"
-        action=""
-        style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={onSubmitHandler}
-      >
-        <h1 className="register">마이페이지 - 내정보
-          <span style={{ color: '#f34336', fontSize: 10, paddingLeft: 10 }}> </span>
-        </h1>
-        <div className="registerbd"></div>
-        <h3 className="rgName">이름
-        </h3>
-        <div>
-          <input
-            className="NameWrite"
-            type='text'
-            name='Name'
-            vlaue={Name}
-            onChange={onNameHandler}
-            placeholder=""
-          />
-        </div>
-        <h3 className="rgID">아이디
-        </h3>
-        <div>
-          <input
-            className="idwrite"
-            type='text'
-            name='Id'
-            value={Id}
-            onChange={onIdHandler}
-            placeholder=""
-          />
-        </div>
+    }}>
+      <h1 className="register">마이페이지 - 내정보</h1>
+      <div className="mt-5 registerbd"></div>
+      
+      <h3 className="rgName">이름 : {localStorage.userName}</h3>
+      <h3 className="rgAddress">주소 : {localStorage.address}</h3>
+      <h3 className="rgPhoneNum">전화번호 : {localStorage.phoneNum}</h3>
 
+      <div className="sidemenu">마이페이지</div>
+      <div className="MyInfo">
+        <Link to="/mypage">
+          <button className="GoMyPage">내 정보</button>
+        </Link>
+      </div>
 
-        <h3 className="rgAddress">주소
-        </h3>
-        <div>
-          <input
-            className="AddressWrite"
-            type='text'
-            name='Address'
-            value={Address}
-            onChange={onAddressHandler}
-            placeholder=""
-          />
-        </div>
-        <h3 className="rgPhoneNum">전화번호
-        </h3>
-        <div>
-          <input
-            className="PhoneNumWrite"
-            type='text'
-            name='PhoneNum'
-            value={PhoneNum}
-            onChange={onPhoneNumHandler}
-            placeholder=""
-          />
-        </div>
-
-        <div className="sidemenu">마이페이지</div>
-
-
-        <div className="MyInfo">
-          <Link to="/mypage">
-            <button className="GoMyPage">내 정보</button>
-          </Link>
-        </div>
-
-        <div className="OrderTable">
-          <Link to="/ordertable">
-            <button className="GoOrderTable">주문 목록</button>
-          </Link>
-        </div>
-
-        <div className="rgbt">
-          <button className="rgbutton">수정하기</button>
-        </div>
-      </form>
+      <div className="OrderTable">
+        <Link to="/ordertable">
+          <button className="GoOrderTable">주문 목록</button>
+        </Link>
+      </div>
     </div>
   )
 }
--- front-end/src/pages/OrderTable.js
+++ front-end/src/pages/OrderTable.js
@@ -1,132 +1,19 @@
 import React from 'react'
 import './OrderTable.css';
 import { Link } from 'react-router-dom';
-import { useState } from 'react'
 
 export default function OrderTable() {
-
-	const [Name, setName] = useState("")
-	const [Id, setId] = useState("")
-	const [Password, setPassword] = useState("")
-	const [PasswordCheck, setPasswordCheck] = useState("")
-	const [Address, setAddress] = useState("")
-	const [PhoneNum, setPhoneNum] = useState("")
-	const [Email, setEmail] = useState("")
-	const [x, setX] = useState([]);
-
-	const onNameHandler = (event) => {
-		setName(event.currentTarget.value)
-	}
-
-	const onIdHandler = (event) => {
-		setId(event.currentTarget.value)
-	}
-
-	const onPasswordHandler = (event) => {
-		setPassword(event.currentTarget.value)
-	}
-
-	const onPasswordCheckHandler = (event) => {
-		setPasswordCheck(event.currentTarget.value)
-	}
-
-	const onAddressHandler = (event) => {
-		setAddress(event.currentTarget.value)
-	}
-
-	const onPhoneNumHandler = (event) => {
-		setPhoneNum(event.currentTarget.value)
-	}
-
-	const onEmailHandler = (event) => {
-		setEmail(event.currentTarget.value)
-	}
-
-	const onSubmitHandler = (event) => {
-		event.preventDefault();
-	}
-
-
-	const HandleClickRadioButton = (e) => {
-		console.log(e.target.value)
-		setX(e.target.value)
-	}
-
+	
 	return (
 		<div style={{
 			display: 'flex', justifyContent: 'center', alignItems: 'center',
 			width: '100%', height: '100vh', overflow: 'scroll'
 		}}
 		>
-			<form
-				className="p-10 space-y-5 bg-white rounded-xl"
-				action=""
-				style={{ display: 'flex', flexDirection: 'column' }}
-				onSubmit={onSubmitHandler}
-			>
 				<h1 className="register">마이페이지 - 주문내역
 					<span style={{ color: '#f34336', fontSize: 10, paddingLeft: 10 }}> </span>
 				</h1>
-				<div className="registerbd"></div>
-				<h3 className="rgName">이름
-				</h3>
-				<div>
-					<input
-						className="NameWrite"
-						type='text'
-						name='Name'
-						vlaue={Name}
-						onChange={onNameHandler}
-						placeholder=""
-					/>
-				</div>
-				<h3 className="rgID">아이디
-				</h3>
-				<div>
-					<input
-						className="idwrite"
-						type='text'
-						name='Id'
-						value={Id}
-						onChange={onIdHandler}
-						placeholder=""
-					/>
-				</div>
-				<div>
-					<input
-						className="PWwrite"
-						type='text'
-						name='Password'
-						value={Password}
-						onChange={onPasswordHandler}
-						placeholder=""
-					/>
-				</div>
 
-				<h3 className="rgAddress">주소
-				</h3>
-				<div>
-					<input
-						className="AddressWrite"
-						type='text'
-						name='Address'
-						value={Address}
-						onChange={onAddressHandler}
-						placeholder=""
-					/>
-				</div>
-				<h3 className="rgPhoneNum">전화번호
-				</h3>
-				<div>
-					<input
-						className="PhoneNumWrite"
-						type='text'
-						name='PhoneNum'
-						value={PhoneNum}
-						onChange={onPhoneNumHandler}
-						placeholder=""
-					/>
-				</div>
 				<div className="sidemenu">마이페이지</div>
 				<div className="MyInfo">
 					<Link to="/mypage">
@@ -140,15 +27,8 @@ export default function OrderTable() {
 					</Link>
 				</div>
 
-				<div className="rgbt">
-					<button className="rgbutton">수정하기</button>
-				</div>
-			</form>
 		</div>
-
-
 	)
-
 }
 
 
