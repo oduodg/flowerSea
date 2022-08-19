9ae4d49050d95db8144b401b5a499af1ed518b78
oduodg <werewildfire@gmail.com>
2022-08-19 21:56:52 +0900

    :art: Login, Logout 구현

--- front-end/src/components/profile.js
+++ front-end/src/components/profile.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export default function Profile() {
 	const location = useLocation();
+	const navigate = useNavigate();
 
-	const userName = localStorage.getItem("userName");
-	
-	if(userName) {
+	const onClickLogout = () => {
+		localStorage.removeItem("userToken");
+		localStorage.removeItem("userName");
+		console.log(localStorage.userToken);
+		return navigate('/')
+	}
+
+	if (!localStorage.getItem("userToken")) {
 		return (
 			<div className="flex justify-center float-right">
-				<Link to="/mypage">
-					<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">{userName}님</div>
+				<Link to="/login">
+					<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">로그인을 해주세요.</div>
 				</Link>
-				<Link to="/mypage">
+				<Link to="/login">
 					<img className="pt-2 mr-4 h-9" alt="profile" src="/images/icon_profile.png" />
 				</Link>
 				<Link to="/cart">
@@ -21,18 +27,19 @@ export default function Profile() {
 			</div>
 		)
 	}
+
 	return (
-		<div className="flex justify-center float-right">
-			<Link to="/login">
-				<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">로그인을 해주세요.</div>
+		<div className="flex float-right">
+			<Link to="/mypage">
+				<div className="text-[#8DA6FF] mt-2.5 mr-4 h-9">{localStorage.getItem("userName")}님</div>
 			</Link>
-			<Link to="/login">
+			<Link to="/mypage">
 				<img className="pt-2 mr-4 h-9" alt="profile" src="/images/icon_profile.png" />
 			</Link>
 			<Link to="/cart">
-				<img className="pt-2 mr-10 h-9" alt="profile" src="/images/icon_cart_color.png" />
+				<img className="pt-2 h-9" alt="profile" src="/images/icon_cart_color.png" />
 			</Link>
+			<button className='h-8 px-1 mt-1.5 text-sm text-gray-300 ml-2 mr-5 border-2 hover:text-[#8DA6FF] hover:border-2 hover:border-[#8DA6FF] rounded-lg' onClick={onClickLogout}>로그아웃</button>
 		</div>
 	)
-
 }
