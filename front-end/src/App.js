240de339a825ae99a125ebfa0eb728ff0b2b939a
이태휘 <taehwi@itaehwis-MacBook-Pro.local>
2022-09-19 18:16:30 +0900

    mypageedit DONE

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Main, Login, Register, Cart, MyPage, Address, OrderTable, Payed } from './pages';
+import { Main, Login, Register, Cart, MyPage, Address, OrderTable, Payed, MyPageEdit } from './pages';
 import Header from './components/header';
 import Order from './pages/Order';
 
@@ -14,6 +14,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypageedit" element={<MyPageEdit />} />
         <Route path="/address" element={<Address />} />
         <Route path="/ordertable" element={<OrderTable />} />
         <Route path="/order" element={<Order />} />
